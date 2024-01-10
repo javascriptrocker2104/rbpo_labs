4202d11c0b6c62874f8d7111f2d927f8ff20de65
javascriptrocker2104 <117441918+javascriptrocker2104@users.noreply.github.com>
2024-01-10 15:49:21 +0300

    Add files via upload

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -1,4 +1,281 @@
-﻿#include <initializer_list>
+﻿#include <iostream>
+#include <initializer_list>
+#include <iterator>
+using namespace std;
+
+template <typename T>
+class StackBasedOnArray
+{
+private:
+    T* arr;
+    int size;
+    const int capacity = 150;
+public:
+    using Iterator = T*;
+
+    StackBasedOnArray();
+    explicit StackBasedOnArray(unsigned size);
+    StackBasedOnArray(const StackBasedOnArray& other);
+    StackBasedOnArray(StackBasedOnArray&& other);
+    StackBasedOnArray(initializer_list<int> list);
+    ~StackBasedOnArray();
+
+    StackBasedOnArray::Iterator begin();
+    const StackBasedOnArray::Iterator begin() const;
+    StackBasedOnArray::Iterator end();
+    const StackBasedOnArray::Iterator end() const;
+
+    const int& operator[](int index) const;
+    int& operator[](int index);
+    StackBasedOnArray& operator=(const StackBasedOnArray& other);
+    StackBasedOnArray& operator=(StackBasedOnArray&& other);
+    StackBasedOnArray& operator=(initializer_list<int> ilist);
+
+    int GetSize() const;
+    bool Push(const T& element);
+    bool Pop(T& element);
+    bool Peek(T& element);
+
+
+};
+template <typename T>
+StackBasedOnArray<T>::StackBasedOnArray() : arr(nullptr), size(0) {}
+template <typename T>
+StackBasedOnArray<T>::StackBasedOnArray(unsigned size) : size(size)
+{
+    arr = new int[size];
+}
+template <typename T>
+StackBasedOnArray<T>::~StackBasedOnArray()
+{
+    if (size > 0)
+        delete[] arr;
+}
+template <typename T>
+StackBasedOnArray<T>::StackBasedOnArray(const StackBasedOnArray& other)
+{
+    arr = new int[other.size];
+    size = other.size;
+    for (int i = 0; i < size; ++i)
+    {
+        arr[i] = other.arr[i];
+    }
+}
+template <typename T>
+StackBasedOnArray<T>::StackBasedOnArray(StackBasedOnArray&& other)
+{
+    arr = other.arr;
+    size = other.size;
+    other.arr = nullptr;
+}
+template <typename T>
+StackBasedOnArray<T>::StackBasedOnArray(std::initializer_list<int> ilist)
+    : arr(new int[ilist.size()]), size(ilist.size())
+{
+    int i = 0;
+    for (const auto& element : ilist)
+    {
+        arr[i++] = element;
+    }
+}
+template <typename T>
+int StackBasedOnArray<T>::GetSize() const
+{
+    return size;
+}
+template <typename T>
+typename StackBasedOnArray<T>::Iterator StackBasedOnArray<T>::begin()
+{
+    return Iterator(arr);
+}
+template <typename T>
+const typename StackBasedOnArray<T>::Iterator StackBasedOnArray<T>::begin() const
+{
+    return Iterator(arr);
+}
+template <typename T>
+typename StackBasedOnArray<T>::Iterator StackBasedOnArray<T>::end()
+{
+    return Iterator(arr + size);
+}
+template <typename T>
+const typename StackBasedOnArray<T>::Iterator StackBasedOnArray<T>::end() const
+{
+    return Iterator(arr + size);
+}
+const int& StackBasedOnArray<int>::operator[](int index) const
+{
+    return arr[index];
+}
+int& StackBasedOnArray<int>::operator[](int index)
+{
+    return arr[index];
+}
+template <typename T>
+StackBasedOnArray<T>& StackBasedOnArray<T>::operator=(const StackBasedOnArray& other)
+{
+    if (this != &other)
+    {
+        delete[] arr;
+        arr = new int[other.size];
+        size = other.size;
+        for (int i = 0; i < size; ++i)
+        {
+            arr[i] = other.arr[i];
+        }
+    }
+    return *this;
+}
+template <typename T>
+StackBasedOnArray<T>& StackBasedOnArray<T>::operator=(StackBasedOnArray&& other)
+{
+    if (this != &other)
+    {
+        delete[] arr;
+        arr = other.arr;
+        size = other.size;
+        other.arr = nullptr;
+    }
+    return *this;
+}
+template <typename T>
+StackBasedOnArray<T>& StackBasedOnArray<T>::operator=(std::initializer_list<int> ilist)
+{
+    delete[] arr;
+    arr = new int[ilist.size()];
+    size = ilist.size();
+    int i = 0;
+    for (const auto& element : ilist)
+    {
+        arr[i++] = element;
+    }
+    return *this;
+}
+template <typename T>
+bool StackBasedOnArray<T>::Push(const T& element)
+{
+    int* temp = arr;
+    if (size < capacity)
+    {
+        arr = new int[size + 1];
+        for (int i = 0; i < size; i++)
+            arr[i] = temp[i];
+
+        arr[size] = element;
+        size++;
+        delete[] temp;
+        return true;
+    }
+    else         
+        return false;
+
+}
+template <typename T>
+bool StackBasedOnArray<T>::Pop(T& element)
+{
+    if (size == 0) {
+        return false;
+    }
+    element = arr[size - 1];
+    arr[size - 1] = T();
+    size--;
+    return true;
+}
+
+template <typename T>
+bool StackBasedOnArray<T>::Peek(T& element)
+{
+    if (size == 0) {
+        return false;
+    }
+    element = arr[size - 1];
+    return true;
+
+}
+
+int main()
+{
+    StackBasedOnArray<int> stack_1;
+    stack_1.Push(9);
+    stack_1.Push(17);
+    stack_1.Push(98);
+
+    cout << "Stack 1: ";
+    for (int* i = stack_1.begin(); i != stack_1.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+    cout << "Size of stack_1: " << stack_1.GetSize() << endl;
+
+    int poppedElement;
+    if (stack_1.Pop(poppedElement))
+        cout << "Popped element: " << poppedElement << endl;
+    else
+        cout << "Stack doesn't have any elements" << endl;
+
+    cout << "Stack 1 after delete: ";
+    for (int* i = stack_1.begin(); i != stack_1.end(); ++i)
+        cout << *i << " ";
+    cout << endl; 
+
+    int peekElement;
+    if (stack_1.Peek(peekElement))
+        cout << "Peek: " << peekElement << endl;
+    else
+        cout << "Stack doesn't have any elements" << endl;
+    cout << endl;
+    
+
+
+    StackBasedOnArray<int> stack_2(0);
+
+    cout << "Stack 2: ";
+    for (int* i = stack_2.begin(); i != stack_2.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+    cout << "Size of stack_2: " << stack_2.GetSize() << endl;
+    stack_2.Push(7);
+    cout << "Stack 2 after add: ";
+    for (int* i = stack_2.begin(); i != stack_2.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+    peekElement = 0;
+    if (stack_2.Peek(peekElement))
+        cout << "Peek: " << peekElement << endl;
+    else
+        cout << "Stack doesn't have any elements" << endl;
+    cout << "Size of stack_2: " << stack_2.GetSize() << endl;
+    cout << endl;
+    
+
+
+    StackBasedOnArray<int> stack_3 = { 21, 32, 43, 54, 65 };
+    cout << "Stack 3: ";
+    for (int* i = stack_3.begin(); i != stack_3.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+    cout << "Size of stack_3: " << stack_3.GetSize() << endl;
+
+    poppedElement=0;
+    if (stack_3.Pop(poppedElement))
+        cout << "Popped element: " << poppedElement << endl;
+    else
+        cout << "Stack doesn't have any elements" << endl;
+    stack_3.Push(4);
+    stack_3.Push(7);
+    cout << "Stack 3 after add 2 elements and delete: ";
+    for (int* i = stack_3.begin(); i != stack_3.end(); ++i)
+        cout << *i << " ";
+    cout << endl;
+    cout << "Size of stack_3: " << stack_3.GetSize() << endl;
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
+/*#include <initializer_list>
 #include <iterator>
 #include<iostream>
 using namespace std;
@@ -262,3 +539,6 @@ int main()
     cout << endl;
     return 0;
 }
+*/
+
+
